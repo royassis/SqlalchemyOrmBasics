8a6ccbd1b23948088033fd2e7a1356ca30efa5e3
Roy <royassisg@gmail.com>
2021-03-22 20:34:28 +0200

    Query with joins

--- sqlalchemyOrmTest.py
+++ sqlalchemyOrmTest.py
@@ -5,25 +5,26 @@ from faker import Faker
 
 faker = Faker()
 odbc_connstr = 'mssql://localhost\SQLEXPRESS/testdb?driver=SQL+Server'
-engine = create_engine(odbc_connstr, echo=True)
+engine = create_engine(odbc_connstr)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 Base.metadata.create_all(engine)
 
-for _ in range(2):
-    first_name = faker.first_name()
-    last_name = faker.last_name()
-    full_name = f"{last_name} {first_name}"
-    mail1 = faker.email()
-    mail2 = faker.email()
+# for _ in range(5):
+#     first_name = faker.first_name()
+#     last_name = faker.last_name()
+#     full_name = f"{last_name} {first_name}"
+#     mail1 = faker.email()
+#     mail2 = faker.email()
+#
+#     user_a = User(name=first_name, fullname=full_name, nickname='nickname')
+#     user_a.addresses = [Address(email_address=mail1), Address(email_address=mail2)]
+#     session.add(user_a)
+#
+# session.commit()
 
-    user_a = User(name=first_name, fullname=full_name, nickname='nickname')
-    user_a.addresses = [Address(email_address=mail1), Address(email_address=mail2)]
-    session.add(user_a)
 
-session.commit()
-
-for user, addresses in session.query(User, Address).filter(Address.user_id == None).order_by(User.id).all():
-    print(user, addresses)
+for i in session.query(User.name,User.id, Address.id).outerjoin(Address).order_by(User.id.desc()).all():
+    print(*i)
